3af1544e49230e663fc6c5acb95bed917a316b31
Zahidali37 <133868612+Zahidali37@users.noreply.github.com>
2023-05-17 16:29:03 +0500

    Add files via upload

---
+++ Qno_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main ()
+{
+
+printf("\033[1;36m\n\n\tName\t\t:Zahid Ali\033[0m\n");
+printf("\033[1;36m\tFather's Name\t:Ameer Hassan\033[0m\n");
+printf("\033[1;36m\tAge\t\t:18 Year's\033[0m\n");
+printf("\033[1;36m\tGrade\t\t:A\033[0m\n");
+
+}
